41a5470d8b811cd7c97f3f00c37b2b4016a910d1
yankikirlikova05 <yanki.kirlikova@gmail.com>
2022-01-08 22:52:02 +0300

    swerve code added to container, navx still broken

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,32 +1,33 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Swerve.SwerveDriveCommand;
+import frc.robot.subsystems.Swerve;
 
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  //Subsystems
+  Swerve swerveDrivetrain = new Swerve(true);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  //Joysticks
+  XboxController driver = new XboxController(0);
+  XboxController operator = new XboxController(1);
+
+  //Commands
+  SwerveDriveCommand driveCommand = new SwerveDriveCommand(swerveDrivetrain, driver);
+
+
   public RobotContainer() {
-    // Configure the button bindings
+    swerveDrivetrain.setDefaultCommand(driveCommand);
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+
   private void configureButtonBindings() {}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+
+
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return null;
